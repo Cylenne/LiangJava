7e19c1d3b652896ce30e2a925b96a48c378c822f
Cylenne <seregi.viktoria@hotmail.com>
2024-04-27 13:57:05 +0200

    first commit

---
+++ src/Methods/Max.java
@@ -0,0 +1,16 @@
+package Methods;
+// 6.1
+public class Max {
+    public static int max(int num1, int num2) {
+        if (num1 > num2) {
+            return num1;
+        }
+        return num2;
+    }
+
+    /* 6.2
+    parameter: variables defined in the method header, like a placeholder
+    argument: when a method is invoked, these are the actual values passed to the parameters
+    method signature: method name + parameters
+     */
+}
